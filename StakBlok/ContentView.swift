fea1a64851fa253f1dabb0d01ea279a4e3da31ad
Arpit Arun Kumaar <arpitak14@outlook.com>
2022-01-10 22:10:39 -0500

    Initial Commit

---
+++ StakBlok/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StakBlok
+//
+//  Created by Arpit Arun Kumaar on 2022-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StakBlok/StakBlokApp.swift
@@ -0,0 +1,17 @@
+//
+//  StakBlokApp.swift
+//  StakBlok
+//
+//  Created by Arpit Arun Kumaar on 2022-01-10.
+//
+
+import SwiftUI
+
+@main
+struct StakBlokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
